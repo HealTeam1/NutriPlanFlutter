e966408706c2d389484c723169d3fe709ddd1ebb
Alejandro Espino <espinofloresa@gmail.com>
2025-06-18 23:45:37 -0500

    chore: Initialized Project

---
+++ README.md
@@ -0,0 +1,3 @@
+# nutrihealth_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/nutrihealth_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nutrihealth_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
