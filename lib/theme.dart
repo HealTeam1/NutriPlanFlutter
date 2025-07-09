572934e3a8350c92fab0a8b17e5ec03b9701826a
Daiki Oscar Oshiro Yamashita <167911449+DaikiOY@users.noreply.github.com>
2025-07-09 11:03:58 -0500

    Create theme.dart

---
+++ lib/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryGreen = Color(0xFF4CAF50); // verde saludable
+  static const Color accentOrange = Color(0xFFFFA726); // naranja claro
+  static const Color background = Color(0xFFF5F5F5); // gris claro
+  static const Color text = Color(0xFF333333); // texto oscuro
+}
